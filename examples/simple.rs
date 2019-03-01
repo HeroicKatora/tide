4166d111d63753bbd3cd296ad8f05ffa76beb2a2
Andreas Molzer <andreas.molzer@gmx.de>
2019-03-01 23:40:46 +0100

    Add the app counter

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,24 +1,30 @@
 #![feature(async_await, futures_api)]
-use std::sync::{atomic::AtomicUsize, Arc};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use tide::{App, IntoResponse, Seeded, Response};
 use tide_session::{GetOrCreate, Store, Session, SessionToken};
 
+#[derive(Clone, Default)]
 struct Counter(Arc<AtomicUsize>);
 
 async fn introspect(token: SessionToken) -> String {
     format!("{:?}", token)
 }
 
-async fn incremental(session: Session<Counter>) -> String {
-    unimplemented!()
+async fn incremental(session: Session<Counter>) -> Response {
+    let previous = session.data().0.fetch_add(1, Ordering::AcqRel);
+    let body = format!("You have been here {} previous times", previous);
+    let mut response = body.into_response();
+    session.attach(&mut response);
+    response
 }
 
 fn main() {
-    let mut app = tide::App::new(());
+    let mut app = App::new(());
     let store = Arc::new(Store::<Counter>::new());
     app.at("/self").get(introspect);
-    // app.at("/").get(Seeded(incremental, GetOrCreate(store)));
+    app.at("/").get(Seeded(incremental, GetOrCreate(store)));
 
     app.serve();
 }
